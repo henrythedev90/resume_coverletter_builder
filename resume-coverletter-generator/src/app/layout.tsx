6a3b0326b19ba61f298852bbcddce147b7b1a843
henrythedev90 <henrythedev90@gmail.com>
2025-03-26 00:58:19 -0400

    trying to figure out css

--- resume-coverletter-generator/src/app/layout.tsx
+++ resume-coverletter-generator/src/app/layout.tsx
@@ -1,6 +1,6 @@
 // import type { Metadata } from "next";
 import Providers from "@/components/Providers";
-import "./globals.css";
+import "../styles/global.css";
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
